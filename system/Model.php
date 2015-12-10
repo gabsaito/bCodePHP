df740fdf03a6cc62090aacfe9656512c67801653
Gabriel Michelini <gabsaito@gmail.com>
2015-12-10 15:40:15 -0200

    Adicionado a classe pai Model

---
+++ system/Model.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace system
+{
+    use system\database\Connection;
+
+    class Model
+    {
+        /**
+         * @var object $connection
+         * Guarda a instância da classe de conexão
+         */
+        public $connection;
+
+        /**
+         * Metódo construtor
+         * Instância a classe Connection
+         */
+        public function __construct()
+        {
+            $this->connection = new Connection();
+        }
+    }
+}
