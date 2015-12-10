a6710f084180fc657c4890093ab25a489ecd10ba
Gabriel Michelini <gabsaito@gmail.com>
2015-12-10 15:29:40 -0200

    Adicionado arquivo da classe que realiza a conexão com o banco de dados

---
+++ system/database/Conexao.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace system\database
+{
+    class Connection
+    {
+        /**
+         * @var object $connection
+         * Guarda a conexão com o banco de dados
+         */
+        private static $connection;
+
+        /**
+         * @return object PDO
+         * Caso exista retorna a conexão com o banco, se não existir realiza a conexão antes de retorna-lá
+         */
+        public static function getConnection()
+        {
+            if(is_null(self::$connection))
+            {
+                try
+                {
+                    $dsn = DB_DRIVER . ':host=' . DB_HOST . ';dbname=' . DB_NAME;
+                    self::$connection = new \PDO($dsn, DB_USER, DB_PASS);
+                    self::$connection->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+                    self::$connection->setAttribute(\PDO::ATTR_DEFAULT_FETCH_MODE, \PDO::FETCH_OBJ);
+                }
+                catch(\PDOException $e)
+                {
+                    echo $e->getMessage();
+                }
+            }
+
+            return self::$connection;
+        }
+    }
+}
