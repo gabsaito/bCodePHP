bd4e514f4960dd460e0770965a1b51142bc9baed
Gabriel Michelini <gabsaito@gmail.com>
2015-12-11 01:24:11 -0200

    Reformulação da classe e alteração de nome de Application para BCodePHP

---
+++ system/core/BCodePHP.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace system\core
+{
+    class BCodePHP
+    {
+        /**
+         * @var object
+         * Contém uma instância da classe URI
+         */
+        private $uri;
+
+        /**
+         * Instância a classe URI
+         */
+        public function __construct()
+        {
+            $this->uri = new URI();
+        }
+
+        /**
+         * Recupera e atribui os valores para o controlador, action e param
+         * Verifica se existe o arquivo do controlador solicidado
+         * Verifica se existe o metódo da action solicidada
+         * Executa o controlador, caso exista
+         * Executa a ação e informa os parâmetros, caso a ação exista
+         */
+        public function start()
+        {
+            $controller = $this->uri->__get('controller');
+            $action     = $this->uri->__get('action');
+            $param      = $this->uri->__get('param');
+
+            if(is_null($controller))
+            {
+                $controller = 'home';
+            }
+            else if(!file_exists(CONTROLLERS_PATH . ucfirst($controller) . 'Controller.php'))
+            {
+                Response::redirectIn('notfound');
+            }
+
+            $instance_controller = '\\application\\controllers\\' . ucfirst($controller) . 'Controller';
+            $class  = new $instance_controller();
+
+            if(is_null($action))
+            {
+                $action = 'index';
+            }
+            else if(!method_exists($class, $action . 'Action'))
+            {
+                Response::redirectIn('notfound');
+            }
+
+            $action = $action . 'Action';
+            $class->$action($param);
+        }
+    }
+}
